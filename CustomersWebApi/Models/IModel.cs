2989a513790ff1fb425052fc19b64840f2f690ba
https://gitlab.com/semmmkp <semmmkp@gmail.com>
2021-04-20 22:04:32 +0300

    Добавлен интерфейс модели

---
+++ CustomersWebApi/Models/IModel.cs
@@ -0,0 +1,24 @@
+﻿using CustomersWebApi.Data;
+using System.Threading.Tasks;
+
+namespace CustomersWebApi.Models
+{
+    /// <summary>
+    /// Интерфейс модели
+    /// </summary>
+    public interface IModel
+    {
+        /// <summary>
+        /// Добавить клиента
+        /// </summary>
+        /// <param name="customer">Добавляемый клиент</param>
+        Task AddCustomerAsync(CustomerDTO customer);
+
+        /// <summary>
+        /// Найти клиента по ID
+        /// </summary>
+        /// <param name="id">ID искомого клиента</param>
+        /// <returns>Найденный клиент</returns>
+        Task<CustomerDTO> FindCustomerByIdAsync(int id);
+    }
+}
