04d5b7e07370a92445e371de113b62984a325cd5
https://gitlab.com/semmmkp <semmmkp@gmail.com>
2021-04-20 22:06:46 +0300

    Добавлена реализация модели для работы с базой данных

---
+++ CustomersWebApi/Models/CustomersDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CustomersWebApi.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CustomersWebApi.Models
+{
+    public class CustomersDbContext : DbContext, IModel
+    {
+        public CustomersDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        /// <inheritdoc/>
+        public async Task AddCustomerAsync(CustomerDTO dto)
+        {
+            await Customers.AddAsync(dto.CreateCustomer());
+            await SaveChangesAsync();
+        }
+
+        /// <inheritdoc/>
+        public async Task<CustomerDTO> FindCustomerByIdAsync(int id)
+        {
+            var result = await Customers.FindAsync(id);
+            return new CustomerDTO(result);
+        }
+    }
+}
